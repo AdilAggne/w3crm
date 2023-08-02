ea6013ade1fb694f939a0c3d04a3c9a4df558649
AdilAggne <129189944+AdilAggne@users.noreply.github.com>
2023-08-02 10:08:30 +0530

    Initial Commit

---
+++ custom-table.js
@@ -0,0 +1,337 @@
+const tableData = [
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 53,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 52,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 58,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "Completed",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 52,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 58,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "Completed",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 53,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 52,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 58,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "Completed",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 52,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 58,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "Completed",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Baitmain",
+    "Project Lead": "Liam Risher",
+    Progress: 70,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 90,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 58,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "Completed",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 52,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "InProgress",
+    "Due Date": "06 Sep 2021",
+  },
+  {
+    "Project Name": "Batman",
+    "Project Lead": "Liam Risher",
+    Progress: 99,
+    Assignee: [
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+      "https://w3crm.dexignzone.com/xhtml/images/contacts/pic1.jpg",
+    ],
+    Status: "Completed",
+    "Due Date": "06 Sep 2021",
+  },
+];
+
+let activeProjPage = 1;
+let maxCount = 5 * activeProjPage;
+
+const setMaxCount = () => {
+  maxCount = 5 * activeProjPage;
+};
+
+const createCustomElements = (type, options) => {
+  if (type === "Project Lead") {
+    const { data, td } = options;
+    const lead = document.createElement("div");
+    const avatar = document.createElement("div");
+    const img = document.createElement("img");
+    const span = document.createElement("span");
+
+    lead.classList.add("lead");
+    avatar.classList.add("avatar");
+    img.src = data["Assignee"][0];
+    span.textContent = data["Project Lead"];
+
+    avatar.appendChild(img);
+    lead.appendChild(avatar);
+    lead.appendChild(span);
+    td.appendChild(lead);
+  }
+  if (type === "Assignee") {
+    const { assignees, td } = options;
+    const stack = document.createElement("div");
+    const avatars = document.createElement("div");
+    let count = 0;
+
+    stack.classList.add("stacked-avatars");
+    avatars.classList.add("avatars");
+
+    assignees.forEach((assignee) => {
+      const avatar = document.createElement("div");
+      const img = document.createElement("img");
+
+      avatar.classList.add("avatar");
+      img.src = assignee;
+      avatar.style.left = `${20 * count}px`;
+
+      avatar.appendChild(img);
+      avatars.appendChild(avatar);
+      stack.appendChild(avatars);
+      td.appendChild(stack);
+      count++;
+    });
+  }
+  if (type === "Progress") {
+    const { status, value, td } = options;
+    const progressBar = document.createElement("div");
+    const progress = document.createElement("progress");
+    const progressValue = document.createElement("span");
+
+    progressBar.classList.add("progress-bar");
+    progress.classList.add("progress");
+    progress.value = value;
+    progress.max = 100;
+    progress.textContent = ` ${value}%`;
+    progressValue.textContent = ` ${value}%`;
+
+    if (status === "Pending" || status === "Inprogress") {
+      progress.classList.add("danger");
+      progressValue.classList.add("danger");
+    } else if (status === "Completed") {
+      progress.classList.add("done");
+      progressValue.classList.add("done");
+    } else {
+      progressValue.classList.add("primary");
+    }
+
+    progressBar.appendChild(progress);
+    progressBar.appendChild(progressValue);
+    td.appendChild(progressBar);
+  }
+};
+
+const createTable = () => {
+  document.querySelector(".active-proj-table table").innerHTML =
+    document.createElement("table").innerHTML;
+  const table = document.querySelector(".active-proj-table table");
+  console.log(table);
+
+  const tbody = document.createElement("tbody");
+
+  const headings = Object.keys(tableData[0]);
+  const tr = document.createElement("tr");
+  headings.forEach((heading) => {
+    const th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = heading;
+    tr.appendChild(th);
+  });
+  tbody.appendChild(tr);
+  let counter = maxCount - 5;
+  for (let i = counter; i < maxCount; i++ && counter++) {
+    const trData = document.createElement("tr");
+    headings.forEach((heading) => {
+      const td = document.createElement("td");
+      if (heading === "Project Lead") {
+        createCustomElements(heading, { data: tableData[i], td });
+      } else if (heading === "Assignee") {
+        createCustomElements(heading, {
+          assignees: tableData[i][heading],
+          td,
+        });
+      } else if (heading === "Progress") {
+        createCustomElements(heading, {
+          td,
+          status: tableData[i].Status,
+          value: tableData[i].Progress,
+        });
+      } else {
+        td.textContent = tableData[i][heading];
+      }
+      trData.appendChild(td);
+    });
+    tbody.appendChild(trData);
+  }
+
+  table.appendChild(tbody);
+  // table.removeChild(table.children[0]);
+};
+
+const prevPage = () => {
+  const navigations = document.querySelectorAll(".nav");
+  if (activeProjPage > 1) {
+    activeProjPage--;
+    Array.from(navigations).forEach((nav) => {
+      nav.classList.remove("selected");
+    });
+    navigations[activeProjPage].classList.add("selected");
+    setMaxCount();
+    createTable();
+  }
+};
+
+const nextPage = () => {
+  const navigations = document.querySelectorAll(".nav");
+  if (activeProjPage < navigations.length - 2) {
+    activeProjPage++;
+    Array.from(navigations).forEach((nav) => {
+      nav.classList.remove("selected");
+    });
+    navigations[activeProjPage].classList.add("selected");
+    setMaxCount();
+    createTable();
+  }
+};
+
+window.addEventListener("load", () => {
+  createTable();
+});
